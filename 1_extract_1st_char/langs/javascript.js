592b81506a7216f1f97132ef96bebc0b0421588c
siddharth <siddharth@socratease.co>
2024-03-26 16:08:19 +0530

    Changed javascript boilerplates to nodejs

--- 1_extract_1st_char/langs/javascript.js
+++ 1_extract_1st_char/langs/javascript.js
@@ -1,9 +1,29 @@
 // User Boilerplate
-function extractFirstChar(str) {
-    //Write your code here
+function extractFirstChar(word){
+	// Write the logic here
 }
 
 // Compiler Boilerplate
-const line = readline();
-const s = extractFirstChar(line);
-print(s);
+let inputBuffer = '';
+let inputLines = '';
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (chunk) => {
+	inputBuffer += chunk;
+});
+process.stdin.on('end', () => {
+	inputLines = inputBuffer.split('\n');
+	inputBuffer = '';
+
+	main();
+});
+
+function readLine() {
+	return inputLines.shift();
+}
+
+function main() {
+	const word = readLine().trim()
+	output = extractFirstChar(word);
+	console.log(output)
+
+}
--- 2_find_value_of_index_array/langs/javascript.js
+++ 2_find_value_of_index_array/langs/javascript.js
@@ -1,14 +1,32 @@
 // User Boilerplate
 function getValueAtIndex(index, arr) {
-    //Write your code here
+    // Write the logic here
     return;
 }
 
 // Compiler Boilerplate
-const numOfElements = parseInt(readline());
-const inputArr = readline()
-    .split(" ")
-    .map((i) => i);
-const index = parseInt(readline());
-const s = getValueAtIndex(index, inputArr);
-print(s);
+let inputBuffer = '';
+let inputLines = '';
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (chunk) => {
+	inputBuffer += chunk;
+});
+process.stdin.on('end', () => {
+	inputLines = inputBuffer.split('\n');
+	inputBuffer = '';
+
+	main();
+});
+
+function readLine() {
+	return inputLines.shift();
+}
+
+function main() {
+	const numOfElements = parseInt(readLine().trim())
+	const inputArr = readLine().split(' ').map((i) => parseInt(i))
+	const index = parseInt(readLine().trim())
+	const s = getValueAtIndex(index, inputArr);
+	console.log(s)
+
+}
